f7e4bb88b78b6ad9f3ec4e798b0b839b22b639a6
Junhyuk Lee <junh071017@gmail.com>
2021-08-08 18:06:23 +0900

    for

--- JSbasics/for/for.js
+++ JSbasics/for/for.js
@@ -4,3 +4,9 @@ while (i < 10) {
     console.log(i);
     i++;
 }
+
+for (i = 0; i < 10; i++) {
+    document.write("Coding Everybody! <br/>")
+}
+document.write("---------------------------------------------------------------- <br/>")
+document.write("Check the console.")
