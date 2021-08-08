1eb2d4eb417c67f8fd22fee50ed3f30b0c25ee6e
Junhyuk Lee <junh071017@gmail.com>
2021-08-08 19:19:07 +0900

    function

---
+++ JSbasics/func/func.js
@@ -0,0 +1,25 @@
+function numbersting() {
+    var i = 0;
+    while (i < 10) {
+        document.write(i);
+        i+=1;
+    }
+}
+
+numbersting();
+
+var i = 0;
+
+for (i = 0; i < 4; i++) {
+    document.write("<br>");
+} 
+
+
+// Function with arguments
+
+function AddTwoNumbers(num1, num2) {
+    document.write(num1 + num2, "<br>");
+}
+
+AddTwoNumbers(10, 20) // 30
+AddTwoNumbers(30, 40) // 70
